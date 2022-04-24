f2b9c7d8739a5d32f88309fc51780e9995549b6f
qsmalljr <qmanjr123@gmail.com>
2022-04-24 11:55:48 -0400

    Test adding some details for Matt

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Matt Kucia</h1>
+                <p>Hi my name is Matt. I am a Mechanical Engineer currently working for a food packaging company.</p>
             </div>
         </div>
         <nav>
